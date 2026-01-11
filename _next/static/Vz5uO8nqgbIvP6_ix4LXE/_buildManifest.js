9702cb6d194864a08d13c51f60ec520eddc94b5e
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2026-01-11 23:26:21 +0100

    Updates

--- _next/static/Vz5uO8nqgbIvP6_ix4LXE/_buildManifest.js
+++ _next/static/Vz5uO8nqgbIvP6_ix4LXE/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/beeApiC//_next/:path+",
+        "destination": "/beeApiC/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
