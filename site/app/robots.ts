bb84fbbd19915b2c274bb76cb9ea2b490c586dad
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2026-01-17 01:29:13 +0100

    feat: config sitemap

--- site/app/robots.ts
+++ site/app/robots.ts
@@ -1,11 +1,14 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://www.bee-apic.fr/sitemap.xml',
+        sitemap: 'https://www.bee-apic.com/sitemap.xml',
     };
 }
--- site/app/sitemap.ts
+++ site/app/sitemap.ts
@@ -1,17 +1,20 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://www.bee-apic.fr',
+            url: 'https://www.bee-apic.com',
             lastModified: new Date(),
         },
         {
-            url: 'https://www.bee-apic.fr/recuperation-essaims',
+            url: 'https://www.bee-apic.com/recuperation-essaims',
             lastModified: new Date(),
         },
         {
-            url: 'https://www.bee-apic.fr/producteur-de-miel',
+            url: 'https://www.bee-apic.com/producteur-de-miel',
             lastModified: new Date(),
         },
     ];
